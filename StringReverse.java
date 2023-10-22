1bbdfe4bb6bcfd4b67a6dc17be71a8f3ec623d6f
Mani babu <manibabu1291@gmail.com>
2023-10-23 02:21:15 +0530

    addition and string 3.1

---
+++ StringReverse.java
@@ -0,0 +1,14 @@
+public class StringReverse {
+
+    public static void main(String [] args){
+
+        String name = "Mani babu";
+        String reverseName = "";
+      
+        for(int i=name.length()-1;i>=0;i--){
+            reverseName +=name.charAt(i);
+        }
+        System.out.println(reverseName);
+    }
+    
+}
